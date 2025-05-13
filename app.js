cb216644bfc44c65eb999ca766d0c2da66df2ffc
redafell <230504553@st.atlas.edu.tr>
2025-05-13 21:56:37 +0300

    Updated character saving and fetch functionality

--- app.js
+++ app.js
@@ -1,71 +1,69 @@
 const http = require('http');
-const port = 3000;
 const fs = require('fs');
 const path = require('path');
-const { error } = require('console');
-const { type } = require('os');
-//think of it as importing laibrary
 
-//getting a chracter
-if(req.url == "chracters" && req.method == "GET") 
-fs.readFile(chracters,'utf-8',(error,data)=>{
-    if(error){
-        res.writeHead("500")
-        res.end("error")
-        return
-    }
-    res.writeHead(200,{'content-type': 'application/json'})
-    res.en(data || '[]')
-})
+const port = 3000;
+const charactersPath = path.join(__dirname, 'characters.json');
 
-else if(req.url == 'chrecters' && req.metho == "POST"){
-    let body = ""
-    req.on('data',chunks => {body += chunks} )
-    req.on('end',() =>{
-    const what_if = json.parse(body)
-    fs.readFile(chracters, "utf-8" , (error , data)=>{
-        let why = []
-        if(!error && data){
-            why = json.parse(data)
-        }
-        why.push(what_if)
-        //writing all the ata back to cheracters
-        fs.writeFile(chracters,json.strigify(why,null,2) , (error)=>{
-            if(erorr){
-                res.writeHead('500')
-                res.end('error')
-                return
+const server = http.createServer((req, res) => {
+    if (req.url === '/characters' && req.method === 'GET') {
+        fs.readFile(charactersPath, 'utf-8', (error, data) => {
+            if (error) {
+                res.writeHead(500);
+                res.end('Error reading file');
+                return;
             }
-            res.writeHead('200',{'content-type':'aplication/json'})
-            res.end(json.strigify({success: true}))
-        } )
-        
-    })    
-}
- )
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(data || '[]');
+        });
+    }
 
-}
+    else if (req.url === '/characters' && req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk;
+        });
 
-else{
-    const server = http.createServer((req, res) => { // this creats the server an have a call back function inside it that takes two parameters even though we never use req 
-        const filepath = path.join(__dirname,'proj.html') // here we simply creat a filepath so we can take the files from irname is built in variable that have the current diractary we "if we har coe the diractary than it will not work on other operating systems " an the other is the file we eant 
-        fs.readFile(filepath, (error, data) => { //we read the file we give it the path than we have a cbf if we couldn't read the content it sens an error if not the error object will be null and the respond will be the data we read from the file 
+        req.on('end', () => {
+            const newCharacter = JSON.parse(body);
+
+            fs.readFile(charactersPath, 'utf-8', (error, data) => {
+                let characters = [];
+                if (!error && data) {
+                    characters = JSON.parse(data);
+                }
+
+                characters.push(newCharacter);
+
+                fs.writeFile(charactersPath, JSON.stringify(characters, null, 2), (error) => {
+                    if (error) {
+                        res.writeHead(500);
+                        res.end('Error writing file');
+                        return;
+                    }
+
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ success: true }));
+                });
+            });
+        });
+    }
+
+    else {
+        // Serve HTML page
+        const filepath = path.join(__dirname, 'cheat.html');
+        fs.readFile(filepath, (error, data) => {
             if (error) {
                 res.writeHead(404);
                 res.end('404 Not Found');
             } else {
-                res.writeHead(200, {'Content-Type': 'text/html'}); // wehn we take the respon we give status code 200 means every thing is alright and tells the page to expect html file coming in 
-                res.write(data);
-                res.end();
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
             }
         });
-    });
-}
-server.listen(port, (error) => {
-    if (error) {
-        console.log('There is an error:', error);
-    } else {
-        console.log('Server is listening on port: ' + port);
     }
 });
-console.log("test")
+
+server.listen(port, () => {
+    console.log('Server is listening on port: ' + port);
+});
